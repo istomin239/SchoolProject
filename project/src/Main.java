b2e897a1bc49e1bc45430b34f7ad4736f0cce60d
istomin239 <paulist@inbox.ru>
2023-04-13 23:50:30 +0300

    add 2, 3

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -5,6 +6,8 @@ public class Main {
         System.out.println("Выберите пункт меню:");
         System.out.println("0 - выход");
         System.out.println("1 - добавить точку");
+        System.out.println("2 - список точек");
+        System.out.println("3 - добавить случайные точки");
     }
 
     static double[] x1 = new double[100];
@@ -39,6 +42,35 @@ public class Main {
         }
     }
 
+    static void show() {
+        System.out.println("Точки группы №1:");
+        for (int i = 0; i < count1; i++) {
+            System.out.printf("%.3f, %.3f\n", x1[i], y1[i]);
+        }
+        System.out.println("Точки группы №2:");
+        for (int i = 0; i < count2; i++) {
+            System.out.printf("%.3f, %.3f\n", x2[i], y2[i]);
+        }
+    }
+
+    static void rnd(Scanner sc) {
+        System.out.println("Введите количество случайных точек, которое хотите добавить: int");
+        int count = sc.nextInt();
+        Random rand = new Random();
+        for (int i = 0; i < count; i++) {
+            if (rand.nextBoolean()) {
+                x1[count1] = rand.nextDouble() * 10 - 5;
+                y1[count1] = rand.nextDouble() * 10 - 5;
+                count1++;
+            } else {
+                x2[count2] = rand.nextDouble() * 10 - 5;
+                y2[count2] = rand.nextDouble() * 10 - 5;
+                count2++;
+            }
+        }
+        System.out.println("Точки добавлены");
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int reqest;
@@ -48,6 +80,10 @@ public class Main {
             switch (reqest) {
                 case 1:
                     add(sc); break;
+                case 2:
+                    show(); break;
+                case 3:
+                    rnd(sc); break;
                 default:
                     System.out.println("Нет такого варианта. Читай внимательней."); break;
             }
